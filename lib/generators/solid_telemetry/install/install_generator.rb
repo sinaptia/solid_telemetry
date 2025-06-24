9e9706396cd8ca33224edb1ce04f6d547c194d1d
Patricio Mac Adden <patriciomacadden@gmail.com>
2025-06-23 22:47:08 -0300

    improve install generator

--- lib/generators/solid_telemetry/install/install_generator.rb
+++ lib/generators/solid_telemetry/install/install_generator.rb
@@ -20,4 +20,40 @@ class SolidTelemetry::InstallGenerator < Rails::Generators::Base
   def schema_version
     "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}"
   end
+
+  def virtual_hostname
+    if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+      "resource#>>'{attributes,host.name}'"
+    elsif defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+      "resource->>'attributes'->>'host.name'"
+    elsif defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+      "JSON_VALUE(resource, '$.attributes.\\\"host.name\\\"')"
+    end
+  end
+
+  def virtual_http_status_code
+    if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) || defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+      "span_attributes->>'http.status_code'"
+    elsif defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+      "span_attributes->'$.\\\"http.status_code\\\"'"
+    end
+  end
+
+  def virtual_instrumentation_scope_name
+    if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) || defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+      "instrumentation_scope->>'name'"
+    elsif defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+      "instrumentation_scope->'$.\\\"name\\\"'"
+    end
+  end
+
+  def virtual_value
+    if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+      "(data_points->0->>'value')::FLOAT"
+    elsif defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
+      "data_points->0->>'value'"
+    elsif defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+      "JSON_VALUE(data_points, '$[0].value' RETURNING FLOAT)"
+    end
+  end
 end
