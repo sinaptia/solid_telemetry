f46a5e9edec4c8fb8d04d2179e6de6618c8a38b2
Patricio Mac Adden <patriciomacadden@gmail.com>
2025-06-25 13:03:31 -0300

    lint

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -65,5 +65,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.solid_telemetry.connects_to = { database: { writing: :telemetry } }
+  config.solid_telemetry.connects_to = {database: {writing: :telemetry}}
 end
