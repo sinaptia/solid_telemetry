e3bdb147a2448cadc807c6708f0777b509590712
Patricio Mac Adden <patriciomacadden@gmail.com>
2025-09-10 16:26:33 -0300

    Don't use Rack::Headers::KNOWN_HEADERS, as it's not available in rack ~> 2.0

--- lib/generators/solid_telemetry/install/templates/config/initializers/opentelemetry.rb
+++ lib/generators/solid_telemetry/install/templates/config/initializers/opentelemetry.rb
@@ -1,6 +1,86 @@
 require "opentelemetry-instrumentation-all"
 require "solid_telemetry/instrumentation/action_pack"
 
+# Extracted from rack: Rack::Headers::KNOWN_HEADERS. Unfortunately, Rails 7.2
+# supports rack >= 2.2.4, which doesn't define Rack::Headers::KNOWN_HEADERS.
+KNOWN_HEADERS = %w[
+  Accept-CH
+  Accept-Patch
+  Accept-Ranges
+  Access-Control-Allow-Credentials
+  Access-Control-Allow-Headers
+  Access-Control-Allow-Methods
+  Access-Control-Allow-Origin
+  Access-Control-Expose-Headers
+  Access-Control-Max-Age
+  Age
+  Allow
+  Alt-Svc
+  Cache-Control
+  Connection
+  Content-Disposition
+  Content-Encoding
+  Content-Language
+  Content-Length
+  Content-Location
+  Content-MD5
+  Content-Range
+  Content-Security-Policy
+  Content-Security-Policy-Report-Only
+  Content-Type
+  Date
+  Delta-Base
+  ETag
+  Expect-CT
+  Expires
+  Feature-Policy
+  IM
+  Last-Modified
+  Link
+  Location
+  NEL
+  P3P
+  Permissions-Policy
+  Pragma
+  Preference-Applied
+  Proxy-Authenticate
+  Public-Key-Pins
+  Referrer-Policy
+  Refresh
+  Report-To
+  Retry-After
+  Server
+  Set-Cookie
+  Status
+  Strict-Transport-Security
+  Timing-Allow-Origin
+  Tk
+  Trailer
+  Transfer-Encoding
+  Upgrade
+  Vary
+  Via
+  WWW-Authenticate
+  Warning
+  X-Cascade
+  X-Content-Duration
+  X-Content-Security-Policy
+  X-Content-Type-Options
+  X-Correlation-ID
+  X-Correlation-Id
+  X-Download-Options
+  X-Frame-Options
+  X-Permitted-Cross-Domain-Policies
+  X-Powered-By
+  X-Redirect-By
+  X-Request-ID
+  X-Request-Id
+  X-Runtime
+  X-UA-Compatible
+  X-WebKit-CS
+  X-XSS-Protection
+]
+
 OpenTelemetry::SDK.configure do |config|
   # By default, there's only one service with the name of the app. If you need
   # to distinguish services (eg. background jobs and the rails server), you can
@@ -17,8 +97,8 @@ OpenTelemetry::SDK.configure do |config|
       span_naming: :class
     },
     "OpenTelemetry::Instrumentation::Rack" => {
-      allowed_request_headers: Rack::Headers::KNOWN_HEADERS.keys,
-      allowed_response_headers: Rack::Headers::KNOWN_HEADERS.keys,
+      allowed_request_headers: KNOWN_HEADERS,
+      allowed_response_headers: KNOWN_HEADERS,
       # we don't want traces coming out of solid_telemetry's controllers
       # if you mount the engine other than /telemetry, make sure you update this block
       untraced_requests: ->(env) {
